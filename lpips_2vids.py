a6b874b61c32da28bc3c42a6a4fb24eba46c1a9f
scribblemaniac <scribblemaniac@users.noreply.github.com>
2023-04-02 23:03:39 -0600

    Move video decoding back to cpu
    Unfortunately torchvision.io.VideoReader doesn't handle variable
    framerates or offset pts very well, so we go back to using
    ffmpeg to decode the videos. This slows things down considerably.

--- lpips_2vids.py
+++ lpips_2vids.py
@@ -2,9 +2,8 @@ import argparse
 import csv
 import gc
 import subprocess
-import sys
 import lpips
-import cv2
+import pymediainfo
 import numpy
 import torch
 from torchvision.io import VideoReader
@@ -29,9 +28,29 @@ def main():
 	if opt.use_gpu:
 		loss_fn.cuda()
 
-	backend = "cuda" if opt.use_gpu else "cpu"
-	reference_decoder = VideoReader(opt.reference, device=backend)
-	distorted_decoder = VideoReader(opt.distorted, device=backend)
+	# Get video metadata
+	reference_info = pymediainfo.MediaInfo.parse(opt.reference)
+	distorted_info = pymediainfo.MediaInfo.parse(opt.distorted)
+	reference_video_tracks = [t for t in reference_info.tracks if t.track_type == 'Video']
+	distorted_video_tracks = [t for t in distorted_info.tracks if t.track_type == 'Video']
+	if len(reference_video_tracks) != 1 or len(distorted_video_tracks) != 1:
+		raise Exception("Expected one video track")
+	reference_width = int(reference_video_tracks[0].video_width)
+	reference_height = int(reference_video_tracks[0].video_height)
+	reference_fps = f"{reference_video_tracks[0].frame_rate_num}/{reference_video_tracks[0].frame_rate_den}"
+	distorted_fps = f"{distorted_video_tracks[0].frame_rate_num}/{distorted_video_tracks[0].frame_rate_den}"
+	distorted_width = int(distorted_video_tracks[0].video_width)
+	distorted_height = int(distorted_video_tracks[0].video_height)
+	if reference_width != distorted_width or reference_height != distorted_height:
+		raise Exception("Video dimensions do not match")
+	if reference_fps != distorted_fps:
+		raise Exception("Video frame rates do not match")
+	WIDTH = reference_width
+	HEIGHT = reference_height
+	FPS = reference_fps
+
+	reference_proc = subprocess.Popen(["/miniconda/bin/ffmpeg", "-i", opt.reference, "-filter_complex", f"[0:v]setpts=PTS-STARTPTS,fps={FPS}[out]", "-map", "[out]", "-pix_fmt", "rgb24", "-c:v", "rawvideo", "-f", "image2pipe", "-"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+	distorted_proc = subprocess.Popen(["/miniconda/bin/ffmpeg", "-i", opt.distorted, "-filter_complex", f"[0:v]setpts=PTS-STARTPTS,fps={FPS}[out]", "-map", "[out]", "-pix_fmt", "rgb24", "-c:v", "rawvideo", "-f", "image2pipe", "-"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
 	try:
 		if opt.out:
@@ -41,21 +60,29 @@ def main():
 
 		count = 0
 		total = 0
-		for reference, distorted in zip(reference_decoder, distorted_decoder):
-			reference = reference["data"][numpy.newaxis, :, :, :]
-			distorted = distorted["data"][numpy.newaxis, :, :, :]
+		while True:
+			reference_raw = reference_proc.stdout.read(WIDTH*HEIGHT*3)
+			if not reference_raw:
+				break
+			reference = numpy.frombuffer(reference_raw, dtype='uint8')
+			reference = reference.reshape((HEIGHT,WIDTH,3))[:,:,:3]
+			reference = lpips.im2tensor(reference)
+			
+			distorted_raw = distorted_proc.stdout.read(WIDTH*HEIGHT*3)
+			if not distorted_raw:
+				break
+			distorted = numpy.frombuffer(distorted_raw, dtype='uint8')
+			distorted = distorted.reshape((HEIGHT,WIDTH,3))[:,:,:3]
+			distorted = lpips.im2tensor(distorted)
+
 			if opt.use_gpu:
-			  reference = reference.permute((0,3,1,2))
-			  distorted = distorted.permute((0,3,1,2))
-				#reference = reference.cuda()
-				#distorted = distorted.cuda()
+				reference = reference.cuda()
+				distorted = distorted.cuda()
 
 			# Compute distance
 			dist = loss_fn.forward(reference, distorted)
 			total += float(dist)
-			#print('Frame %d: %.3f'%(count, dist))
 			writer.writerow([count, float(dist)])
-			#f.flush()
 
 			del reference
 			del distorted
