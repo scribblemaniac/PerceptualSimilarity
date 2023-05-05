99094b83463f58659c7a4feb9c0a93b56c22e5b2
scribblemaniac <scribblemaniac@users.noreply.github.com>
2023-05-05 04:20:27 -0600

    Add resume functionality

--- lpips_2vids.py
+++ lpips_2vids.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 import gc
+import os.path
 import subprocess
 import lpips
 import pymediainfo
@@ -17,6 +18,7 @@ def parse_args():
 	parser.add_argument('-v','--version', type=str, default='0.1')
 	parser.add_argument('-n', '--net', type=str, choices=['alex', 'squeeze', 'vgg'], default='alex')
 	parser.add_argument('--use_gpu', action='store_true', help='turn on flag to use GPU')
+	parser.add_argument('--no-resume', action='store_false', dest="resume", help='overwrite existing output instead of continuing it')
 
 	return parser.parse_args()
 
@@ -32,42 +34,51 @@ def main():
 	# Get video metadata
 	reference_info = pymediainfo.MediaInfo.parse(opt.reference)
 	distorted_info = pymediainfo.MediaInfo.parse(opt.distorted)
-	
+
 	reference_video_tracks = [t for t in reference_info.tracks if t.track_type == 'Video']
 	distorted_video_tracks = [t for t in distorted_info.tracks if t.track_type == 'Video']
 	if len(reference_video_tracks) != 1 or len(distorted_video_tracks) != 1:
 		raise Exception("Expected one video track")
-	
+
 	reference_width = int(reference_video_tracks[0].width)
 	reference_height = int(reference_video_tracks[0].height)
 	reference_fps = f"{reference_video_tracks[0].framerate_num}/{reference_video_tracks[0].framerate_den}"
-	
+
 	distorted_width = int(distorted_video_tracks[0].width)
 	distorted_height = int(distorted_video_tracks[0].height)
 	distorted_fps = f"{distorted_video_tracks[0].framerate_num}/{distorted_video_tracks[0].framerate_den}"
-	
+
 	if reference_width != distorted_width or reference_height != distorted_height:
 		raise Exception("Video dimensions do not match")
 	if reference_fps != distorted_fps:
 		raise Exception("Video frame rates do not match")
-		
+
 	WIDTH = reference_width
 	HEIGHT = reference_height
 	FPS = reference_fps
 
-	reference_proc = subprocess.Popen(["/miniconda/bin/ffmpeg", "-i", opt.reference, "-filter_complex", f"[0:v]setpts=PTS-STARTPTS,fps={FPS}[out]", "-map", "[out]", "-pix_fmt", "rgb24", "-c:v", "rawvideo", "-f", "image2pipe", "-"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-	distorted_proc = subprocess.Popen(["/miniconda/bin/ffmpeg", "-i", opt.distorted, "-filter_complex", f"[0:v]setpts=PTS-STARTPTS,fps={FPS}[out]", "-map", "[out]", "-pix_fmt", "rgb24", "-c:v", "rawvideo", "-f", "image2pipe", "-"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-	
 	pbar = tqdm.tqdm(total=int(reference_info.tracks[0].frame_count))
 
 	try:
-		if opt.out:
-			f = open(opt.out,'w', newline='')
-			writer = csv.writer(f)
-			writer.writerow(["Frame",opt.net])
-
-		count = 0
 		total = 0
+		count = 0
+		if opt.out:
+			if os.path.exists(opt.out):
+				with open(opt.out, 'r') as f:
+					reader = csv.DictReader(f)
+					for row in reader:
+						total += float(row[opt.net])
+						count += 1
+			f = open(opt.out, 'w' if count == 0 else 'a', newline='')
+			writer = csv.writer(f)
+			if count == 0:
+				writer.writerow(["Frame",opt.net])
+
+		reference_proc = subprocess.Popen(["/miniconda/bin/ffmpeg", "-i", opt.reference, "-filter_complex", f"[0:v]setpts=PTS-STARTPTS,fps={FPS},select=gte(n\,{count})[out]", "-vsync", "0", "-map", "[out]", "-pix_fmt", "rgb24", "-c:v", "rawvideo", "-f", "image2pipe", "-"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+		distorted_proc = subprocess.Popen(["/miniconda/bin/ffmpeg", "-i", opt.distorted, "-filter_complex", f"[0:v]setpts=PTS-STARTPTS,fps={FPS},select=gte(n\,{count})[out]", "-vsync", "0", "-map", "[out]", "-pix_fmt", "rgb24", "-c:v", "rawvideo", "-f", "image2pipe", "-"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+		pbar.update(count)
+
 		while True:
 			reference_raw = reference_proc.stdout.read(WIDTH*HEIGHT*3)
 			if not reference_raw:
@@ -75,7 +86,7 @@ def main():
 			reference = numpy.frombuffer(reference_raw, dtype='uint8')
 			reference = reference.reshape((HEIGHT,WIDTH,3))[:,:,:3]
 			reference = lpips.im2tensor(reference)
-			
+
 			distorted_raw = distorted_proc.stdout.read(WIDTH*HEIGHT*3)
 			if not distorted_raw:
 				break
